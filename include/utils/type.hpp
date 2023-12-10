bf569fb62b32116256fc9e360bc45c0af4ba8506
Victor Arseniu <victor.arseniu@gmail.com>
2023-12-10 19:29:14 +0200

    Added basic type traits and allocator
    Added type traits like relocatable, fundamental, trivially
    destructible/copyable, and basic allocator class.
    TODO: consider benefits/drawbacks of other allocation libraries in
    contrast to libc.

--- include/utils/type.hpp
+++ include/utils/type.hpp
@@ -6,7 +6,9 @@
  */
 #pragma once
 
+#include <cstdlib>
 #include <cstdint>
+#include <utility>
 
 /** Byte type. */
 typedef ::std::uint8_t byte;
@@ -41,5 +43,88 @@ typedef ::std::ptrdiff_t size;
 
 namespace utils
 {
+    /// @brief Concept that classifies a fundamental type.
+    /// @tparam type The type checked.
+    template<typename type>
+    concept fundamental = ::std::is_fundamental_v<type>;
+
+    /// @brief Concept that classifies a trivially copyable type.
+    /// @tparam type The type checked.
+    template<typename type>
+    concept trivially_copyable = ::std::is_trivially_copyable_v<type>;
+
+    /// @brief Concept that classifies a trivially destructible type.
+    /// @tparam type The type checked.
+    template<typename type>
+    concept trivially_destructible = ::std::is_trivially_destructible_v<type>;
+
+    /// @brief A struct boolean type that classifies relocatable types.
+    /// @tparam type The type checked.
+    ///
+    /// A relocatable type is a type for which objects can be moved around by simply
+    /// moving their bytes (for example using memcpy).
+    ///
+    /// By default, all types are classified as non-relocatable, other than fundamental
+    /// or trivially copyable types. It is recommended to create specializations as it
+    /// can make certain algorithms faster.
+    template<typename type>
+    struct is_relocatable : public ::std::false_type {};
+
+    template<typename type> requires (fundamental<type> || trivially_copyable<type>)
+    struct is_relocatable<type> : public ::std::true_type {};
+
+    /// @brief Concept that classifies a relocatable type.
+    /// @tparam type The type checked.
+    template<typename type>
+    concept relocatable = is_relocatable<type>::value;
+
+    /// @brief An allocator type used for handling memory allocation/deallocation,
+    /// and object construction/destruction.
+    /// @tparam type The type that the allocator handles.
+    template<typename type>
+    class allocator
+    {
+    public:
+        /** The type allocated by the allocator. */
+        typedef type value_type;
+        /** The type of the allocator. */
+        typedef allocator<type> allocator_type;
+
+        allocator() = delete;
+        ~allocator() = delete;
+
+        /// @brief Allocates a single object.
+        /// @return Pointer to the object allocated, or nullptr if failed.
+        static inline value_type* allocate() noexcept { return reinterpret_cast<value_type*>(::std::malloc(sizeof(value_type))); }
+        /// @brief Allocates a contiguous array of objects.
+        /// @param n The length of the array to allocate.
+        /// @return Pointer to the beginning of the array allocated, or nullptr if failed.
+        static inline value_type* allocate(usize n) noexcept { return reinterpret_cast<value_type*>(::std::malloc(n * sizeof(value_type))); }
+
+        /// @brief Reallocates a contiguous array of objects.
+        /// @param ptr The beginning of the array to reallocate.
+        /// @param n The length of the reallocated array.
+        /// @return Pointer to the beginning of the array allocated, or nullptr if failed.
+        static inline value_type* reallocate(value_type* ptr, usize n) noexcept { return reinterpret_cast<value_type*>(::std::realloc(ptr, n * sizeof(value_type))); }
+
+        /// @brief Deallocates a memory region previously allocated.
+        /// @param ptr The beginning of the region to deallocate.
+        static inline void deallocate(value_type* ptr) noexcept { ::std::free(ptr); }
+
+        /// @brief In-place constructs an object.
+        /// @param ptr The memory location where to construct the object.
+        /// @param _args The arguments to forward to the object's constructor.
+        template<typename... args>
+        static inline void construct_at(value_type* ptr, args&&... _args) noexcept { new (ptr) value_type(::std::forward<args>(_args)...); }
+
+        /// @brief In-place destructs an object.
+        /// @param ptr The memory location of the object to destruct.
+        ///
+        /// For trivially destructible types, it is specialized to do nothing (i.e. it gets
+        /// compiled away).
+        static inline void destruct_at(value_type* ptr) noexcept { ptr->~value_type(); }
+
+        static inline void destruct_at([[maybe_unused]] value_type* ptr) noexcept requires trivially_destructible<value_type> {}
+    };
 };
 
--- tests/utils/typetest.cpp
+++ tests/utils/typetest.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <utils/type.hpp>
 
-TEST_CASE( "type.hpp check", "[type]" )
+TEST_CASE("basic type check", "[type]")
 {
     REQUIRE(sizeof(byte) == 1);
 
@@ -17,5 +17,85 @@ TEST_CASE( "type.hpp check", "[type]" )
 
     REQUIRE(sizeof(f32) == 4);
     REQUIRE(sizeof(f64) == 8);
+
+    REQUIRE(utils::relocatable<u8> == true);
+    REQUIRE(utils::relocatable<f32> == true);
+
+    REQUIRE(utils::trivially_copyable<i8> == true);
+    REQUIRE(utils::trivially_copyable<i32> == true);
+
+    REQUIRE(utils::trivially_destructible<f64> == true);
+    REQUIRE(utils::trivially_destructible<u64> == true);
+}
+
+class ref_counter
+{
+public:
+    inline ref_counter() noexcept
+    {
+        count++;
+    }
+
+    inline ref_counter(const ref_counter& other) noexcept
+    {
+        count++;
+    }
+
+    inline ref_counter(ref_counter&& other) noexcept
+    {
+        count++;
+    }
+
+    inline ~ref_counter() noexcept
+    {
+        count--;
+    }
+
+    static inline int get() noexcept { return count; }
+
+private:
+    static inline int count = 0;
+};
+
+TEST_CASE("allocator check", "[type]")
+{
+    int *v;
+
+    v = utils::allocator<int>::allocate();
+    REQUIRE(v != nullptr);
+    utils::allocator<int>::deallocate(v);
+
+    v = utils::allocator<int>::allocate(1'000'000);
+    REQUIRE(v != nullptr);
+    
+    v = utils::allocator<int>::reallocate(v, 10'000'000);
+    REQUIRE(v != nullptr);
+
+    utils::allocator<int>::deallocate(v);
+
+    v = utils::allocator<int>::allocate();
+
+    utils::allocator<int>::construct_at(v, 10);
+    REQUIRE(*v == 10);
+
+    utils::allocator<int>::deallocate(v);
+
+    ref_counter *counter;
+    counter = utils::allocator<ref_counter>::allocate();
+    REQUIRE(ref_counter::get() == 0);
+
+    utils::allocator<ref_counter>::construct_at(counter);
+    REQUIRE(ref_counter::get() == 1);
+
+    utils::allocator<ref_counter>::destruct_at(counter);
+    REQUIRE(ref_counter::get() == 0);
+
+    utils::allocator<ref_counter>::deallocate(counter);
+    REQUIRE(ref_counter::get() == 0);
+
+    counter = utils::allocator<ref_counter>::allocate();
+    utils::allocator<ref_counter>::construct_at(counter);
+    utils::allocator<ref_counter>::deallocate(counter);
+    REQUIRE(ref_counter::get() == 1);
 }
 
