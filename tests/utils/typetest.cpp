8a93324d17645b6631d9b75c29fc320158fc7f68
Victor Arseniu <victor.arseniu@gmail.com>
2023-12-10 18:19:13 +0200

    Removed 128-bit integers
    __int128_t and __uint128_t are only provided by GCC, for specific
    platforms. i128 and u128 have been removed since they barely have any
    use cases anyway.

--- tests/utils/typetest.cpp
+++ tests/utils/typetest.cpp
@@ -10,13 +10,11 @@ TEST_CASE( "type.hpp check", "[type]" )
     REQUIRE(sizeof(u16) == 2);
     REQUIRE(sizeof(u32) == 4);
     REQUIRE(sizeof(u64) == 8);
-    REQUIRE(sizeof(u128) == 16);
     
     REQUIRE(sizeof(i8) == 1);
     REQUIRE(sizeof(i16) == 2);
     REQUIRE(sizeof(i32) == 4);
     REQUIRE(sizeof(i64) == 8);
-    REQUIRE(sizeof(i128) == 16);
 
     REQUIRE(sizeof(f32) == 4);
     REQUIRE(sizeof(f64) == 8);
